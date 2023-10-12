23f3bc4b4f2de09b09df590e203826f2b199dd7d
Pulkit Sachdeva <sachdeva.pulkit2599@gmail.com>
2023-10-13 04:53:38 +0530

    initial commit

---
+++ src/screens/LandingPageDesktop/LandingPageDesktop.jsx
@@ -0,0 +1,217 @@
+import React from "react";
+import { ExploreNow } from "../../components/ExploreNow";
+import "./style.css";
+
+export const LandingPageDesktop = () => {
+  return (
+    <div className="landing-page-desktop">
+      <div className="div">
+        <div className="overlap">
+          <div className="rectangle" />
+          <img className="screenshot" alt="Screenshot" src="/img/screenshot-669-1.png" />
+          <img className="img" alt="Rectangle" src="/img/rectangle-404.svg" />
+          <div className="rectangle-2" />
+          <img className="group" alt="Group" src="/img/group-289.png" />
+          <ExploreNow className="explore-now-instance" />
+          <p className="discover-the-best">
+            <span className="span">
+              Discover the <br />
+            </span>
+            <span className="text-wrapper-2">Best</span>
+            <span className="span"> Food and Drinks</span>
+          </p>
+          <p className="naturally-made">
+            Naturally made Healthcare Products for the better care &amp; support of your body.
+          </p>
+          <img className="about-girl-lp" alt="About girl lp" src="/img/about-girl-lp.png" />
+          <div className="group-2" />
+          <img className="vector" alt="Vector" src="/img/vector-1.svg" />
+          <img className="group-3" alt="Group" src="/img/group-318.png" />
+          <div className="group-4">
+            <div className="text-wrapper-3">Latest Articles</div>
+            <div className="group-5">
+              <img className="grilled-tomatoes" alt="Grilled tomatoes" src="/img/grilled-tomatoes-1-846x846-1.png" />
+              <div className="frame">
+                <div className="frame-2">
+                  <div className="text-wrapper-4">Grilled&nbsp;&nbsp;Tomatoes at Home</div>
+                  <p className="plorem-ipsum-is">
+                    PLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+                    industry&#39;s standard...
+                  </p>
+                </div>
+                <img className="group-6" alt="Group" src="/img/group-297-4.png" />
+              </div>
+            </div>
+            <div className="overlap-group-wrapper">
+              <div className="overlap-group">
+                <div className="frame-wrapper">
+                  <div className="frame-3">
+                    <div className="frame-4">
+                      <div className="text-wrapper-5">Snacks for Travel</div>
+                      <p className="plorem-ipsum-is">
+                        PLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been
+                        the industry&#39;s standard...
+                      </p>
+                    </div>
+                    <img className="group-6" alt="Group" src="/img/group-297-1.png" />
+                  </div>
+                </div>
+                <img className="meal-prep-ideas" alt="Meal prep ideas" src="/img/meal-prep-ideas-846x846-1.png" />
+              </div>
+            </div>
+            <div className="overlap-wrapper">
+              <div className="overlap-group">
+                <div className="frame-wrapper">
+                  <div className="frame-3">
+                    <div className="frame-4">
+                      <div className="text-wrapper-5">Post-workout Recipes</div>
+                      <p className="plorem-ipsum-is">
+                        PLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been
+                        the industry&#39;s standard...
+                      </p>
+                    </div>
+                    <img className="group-7" alt="Group" src="/img/group-297-2.png" />
+                  </div>
+                </div>
+                <img className="meal-prep-ideas" alt="Meal prep ideas" src="/img/meal-prep-ideas-846x846-1-1.png" />
+              </div>
+            </div>
+          </div>
+          <div className="frame-5">
+            <div className="div-wrapper">
+              <div className="frame-6">
+                <div className="text-wrapper-6">About Us</div>
+                <p className="lorem-ipsum-is">
+                  Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+                  industry&#39;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type
+                  and scrambled it to make a type specimen book. t has survived not only five centuries.
+                </p>
+              </div>
+            </div>
+            <img className="group-8" alt="Group" src="/img/group-297-3.png" />
+          </div>
+          <div className="text-wrapper-7">1/2</div>
+          <div className="group-9">
+            <div className="overlap-2">
+              <img className="image-traced" alt="Image traced" src="/img/image-1-traced.svg" />
+              <div className="rectangle-3" />
+            </div>
+          </div>
+          <div className="group-10">
+            <div className="overlap-2">
+              <img className="image-traced" alt="Image traced" src="/img/image-1-traced-4.svg" />
+              <img className="image-traced" alt="Image traced" src="/img/image-1-traced-4.svg" />
+              <div className="rectangle-4" />
+            </div>
+          </div>
+        </div>
+        <div className="overlap-3">
+          <div className="group-11">
+            <div className="overlap-4">
+              <div className="group-12">
+                <div className="group-13">
+                  <div className="text-wrapper-8">Contact Us</div>
+                  <div className="group-14">
+                    <p className="p">
+                      Lorem Ipsum Pvt Ltd.5/1, Magalton Road, Phartosh Gate near YTM Market, XYZ-343434
+                    </p>
+                    <div className="text-wrapper-9">example2020@gmail.com</div>
+                    <div className="text-wrapper-10">(904) 443-0343</div>
+                  </div>
+                </div>
+                <div className="overlap-group-2">
+                  <div className="group-15">
+                    <div className="text-wrapper-11">More</div>
+                    <div className="text-wrapper-12">Social Links</div>
+                    <div className="group-16">
+                      <div className="text-wrapper-13">Products</div>
+                      <div className="text-wrapper-14">Career</div>
+                      <div className="text-wrapper-15">Contact Us</div>
+                      <div className="text-wrapper-16">About Us</div>
+                    </div>
+                  </div>
+                  <div className="group-17">
+                    <p className="text-wrapper-17">© 2022 Food Truck Example</p>
+                  </div>
+                </div>
+              </div>
+              <img className="component" alt="Component" src="/img/component.png" />
+              <div className="component-2">
+                <img className="rectangle-5" alt="Rectangle" src="/img/rectangle-393.png" />
+                <img className="path" alt="Path" src="/img/path-2.svg" />
+              </div>
+              <img className="component-3" alt="Component" src="/img/component-1.png" />
+            </div>
+          </div>
+          <img className="screenshot-2" alt="Screenshot" src="/img/screenshot-669-2.png" />
+        </div>
+        <div className="overlap-5">
+          <div className="text-wrapper-18">2/2</div>
+          <div className="group-18">
+            <div className="overlap-2">
+              <img className="image-traced" alt="Image traced" src="/img/image-1-traced-3.svg" />
+              <div className="rectangle-4" />
+            </div>
+          </div>
+          <div className="group-19">
+            <div className="overlap-2">
+              <img className="image-traced" alt="Image traced" src="/img/image-1-traced-4.svg" />
+              <img className="image-traced" alt="Image traced" src="/img/image-1-traced-5.svg" />
+              <div className="rectangle-3" />
+            </div>
+          </div>
+          <div className="group-20">
+            <div className="text-wrapper-3">Latest Articles</div>
+            <div className="group-5">
+              <img className="grilled-tomatoes" alt="Grilled tomatoes" src="/img/grilled-tomatoes-1-846x846-1-1.png" />
+              <div className="frame">
+                <div className="frame-2">
+                  <div className="text-wrapper-4">How To Grill Corn</div>
+                  <p className="plorem-ipsum-is">
+                    PLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+                    industry&#39;s standard...
+                  </p>
+                </div>
+                <img className="group-6" alt="Group" src="/img/group-297-4.png" />
+              </div>
+            </div>
+            <div className="overlap-group-wrapper">
+              <div className="overlap-group">
+                <div className="frame-wrapper">
+                  <div className="frame-3">
+                    <div className="frame-4">
+                      <div className="text-wrapper-5">Crunchwrap Supreme</div>
+                      <p className="plorem-ipsum-is">
+                        PLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been
+                        the industry&#39;s standard...
+                      </p>
+                    </div>
+                    <img className="group-6" alt="Group" src="/img/group-297-5.png" />
+                  </div>
+                </div>
+                <img className="meal-prep-ideas" alt="Meal prep ideas" src="/img/meal-prep-ideas-846x846-1-2.png" />
+              </div>
+            </div>
+            <div className="overlap-wrapper">
+              <div className="overlap-group">
+                <div className="frame-wrapper">
+                  <div className="frame-3">
+                    <div className="frame-4">
+                      <div className="text-wrapper-5">Broccoli Cheese Soup</div>
+                      <p className="plorem-ipsum-is">
+                        PLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been
+                        the industry&#39;s standard...
+                      </p>
+                    </div>
+                    <img className="group-7" alt="Group" src="/img/group-297-6.png" />
+                  </div>
+                </div>
+                <img className="meal-prep-ideas" alt="Meal prep ideas" src="/img/meal-prep-ideas-846x846-1-3.png" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
---
+++ src/screens/LandingPagePhone/index.js
@@ -0,0 +1,173 @@
+import React from "react";
+import { ExploreNow } from "../../components/ExploreNow";
+import "./index.css";
+
+export const LandingPageMobile = () => {
+  return (
+    <div className="landing-page-mobile">
+      <div className="div">
+        <div className="overlap">
+          <div className="group">
+            <p className="lorem-ipsum-is">
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+              industry&#39;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and
+              scrambled it to make a type specimen book. t has survived not only five centuries.
+            </p>
+            <div className="text-wrapper-2">About Us</div>
+            <div className="div-wrapper">
+              <div className="text-wrapper-3">Read More</div>
+            </div>
+          </div>
+        </div>
+        <div className="overlap-group">
+          <div className="rectangle" />
+          <div className="text-wrapper-4">1/2</div>
+          <div className="overlap-group-wrapper">
+            <div className="overlap-group-2">
+              <p className="plorem-ipsum-is">
+                PLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+                industry&#39;s standard dummy text ever since the 1500s, when an unknown printer took a galley....
+              </p>
+              <div className="grilled-tomatoes-at">Grilled&nbsp;&nbsp;Tomatoes at Home</div>
+              <img className="img" alt="Group" src="group-297.png" />
+            </div>
+          </div>
+          <img className="grilled-tomatoes" alt="Grilled tomatoes" src="grilled-tomatoes-1-846x846-3.png" />
+          <div className="overlap-wrapper">
+            <div className="overlap-group-2">
+              <p className="plorem-ipsum-is">
+                PLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+                industry&#39;s standard dummy text ever since the 1500s, when an unknown printer took a galley....
+              </p>
+              <div className="text-wrapper-5">Snacks for Travel</div>
+              <img className="img" alt="Group" src="image.png" />
+            </div>
+          </div>
+          <img className="grilled-tomatoes-2" alt="Grilled tomatoes" src="grilled-tomatoes-1-846x846-4.png" />
+          <div className="group-2">
+            <div className="overlap-group-2">
+              <p className="p">
+                PLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+                industry&#39;s standard dummy text ever since the 1500s, when an unknown printer took a galley....
+              </p>
+              <div className="text-wrapper-6">Post-workout Recipes</div>
+              <img className="img" alt="Group" src="group-297-2.png" />
+            </div>
+          </div>
+          <img className="grilled-tomatoes-3" alt="Grilled tomatoes" src="grilled-tomatoes-1-846x846-5.png" />
+          <div className="text-wrapper-7">Latest Articles</div>
+          <div className="image-traced-wrapper">
+            <img className="image-traced" alt="Image traced" src="image-1-traced-3.svg" />
+          </div>
+          <div className="img-wrapper">
+            <img className="image-traced-2" alt="Image traced" src="image-1-traced-2.svg" />
+          </div>
+        </div>
+        <p className="discover-the-best">
+          <span className="span">
+            Discover the <br />
+          </span>
+          <span className="text-wrapper-8">Best</span>
+          <span className="span"> Food and Drinks</span>
+        </p>
+        <p className="naturally-made">
+          Naturally made Healthcare Products for the better care &amp; support of your body.
+        </p>
+        <ExploreNow className="explore-now-instance" divClassName="design-component-instance-node" />
+        <div className="overlap-2">
+          <img className="group-3" alt="Group" src="group-319.png" />
+          <div className="group-wrapper">
+            <img className="group-4" alt="Group" src="group-318.png" />
+          </div>
+        </div>
+        <div className="overlap-3">
+          <div className="group-5">
+            <div className="text-wrapper-9">Contact Us</div>
+            <div className="group-6">
+              <div className="overlap-group-3">
+                <p className="text-wrapper-10">
+                  Lorem Ipsum Pvt Ltd.5/1, Magalton Road, Phartosh Gate near YTM Market, XYZ-343434
+                </p>
+                <div className="text-wrapper-11">example2020@gmail.com</div>
+                <div className="text-wrapper-12">(904) 443-0343</div>
+              </div>
+            </div>
+          </div>
+          <div className="group-7">
+            <div className="overlap-4">
+              <div className="text-wrapper-13">More</div>
+              <div className="group-8">
+                <div className="overlap-group-4">
+                  <div className="text-wrapper-14">Products</div>
+                  <div className="text-wrapper-15">Career</div>
+                  <div className="text-wrapper-16">Contact Us</div>
+                  <div className="text-wrapper-17">About Us</div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="group-9">
+            <p className="text-wrapper-18">© 2022 Food Truck Example</p>
+          </div>
+          <div className="screenshot-wrapper">
+            <img className="screenshot" alt="Screenshot" src="screenshot-669-5.png" />
+          </div>
+          <img className="component" alt="Component" src="component.png" />
+          <img className="component-2" alt="Component" src="component-2.png" />
+          <img className="component-3" alt="Component" src="component-3.png" />
+        </div>
+        <div className="group-10">
+          <div className="overlap-5">
+            <div className="text-wrapper-19">2/2</div>
+            <div className="overlap-6">
+              <div className="group-11">
+                <div className="overlap-group-5">
+                  <p className="plorem-ipsum-is-2">
+                    PLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+                    industry&#39;s standard dummy text ever since the 1500s, when an unknown printer took a galley....
+                  </p>
+                  <div className="text-wrapper-20">How To Grill Corn</div>
+                  <img className="group-12" alt="Group" src="group-297-3.png" />
+                </div>
+              </div>
+              <img className="grilled-tomatoes-4" alt="Grilled tomatoes" src="grilled-tomatoes-1-846x846-6.png" />
+            </div>
+            <div className="overlap-7">
+              <div className="group-11">
+                <div className="overlap-group-5">
+                  <p className="plorem-ipsum-is-2">
+                    PLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+                    industry&#39;s standard dummy text ever since the 1500s, when an unknown printer took a galley....
+                  </p>
+                  <div className="text-wrapper-21">Crunchwrap Supreme</div>
+                  <img className="group-12" alt="Group" src="group-297-4.png" />
+                </div>
+              </div>
+              <img className="grilled-tomatoes-4" alt="Grilled tomatoes" src="grilled-tomatoes-1-846x846-7.png" />
+            </div>
+            <div className="overlap-8">
+              <div className="group-11">
+                <div className="overlap-group-5">
+                  <p className="plorem-ipsum-is-3">
+                    PLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+                    industry&#39;s standard dummy text ever since the 1500s, when an unknown printer took a galley....
+                  </p>
+                  <div className="text-wrapper-22">Broccoli Cheese Soup</div>
+                  <img className="group-12" alt="Group" src="group-297-5.png" />
+                </div>
+              </div>
+              <img className="grilled-tomatoes-4" alt="Grilled tomatoes" src="grilled-tomatoes-1-846x846-8.png" />
+            </div>
+            <div className="text-wrapper-23">Latest Articles</div>
+            <div className="group-13">
+              <img className="image-traced" alt="Image traced" src="image-1-traced.svg" />
+            </div>
+            <div className="group-14">
+              <img className="image-traced-2" alt="Image traced" src="image.svg" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
